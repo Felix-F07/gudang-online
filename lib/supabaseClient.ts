fae7c187d22ee94bae8fe837ba6ee2efd3096d80
Felix-F07 <felixff16107@gmail.com>
2025-10-16 14:18:20 +0700

    Use environment variables for Supabase keys

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Ganti dengan URL & Key dari dashboard Supabase Anda
-const supabaseUrl = 'https://effcfuogblhcoxwfwexl.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmZmNmdW9nYmxoY294d2Z3ZXhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MDUwNTgsImV4cCI6MjA3NjA4MTA1OH0.yElPEpxHmjSFXDVQg8CEHDG4yWyMQPrzWBxg7OI3Yh8';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
